580ea3e7ca0c9e44581aa7c77fc68f4128b656c9
boba <boba@10.18.149.57>
2016-06-03 13:25:28 -0600

    5/12/2016 Character 5/14/2016 Item, Map, Game 5/26/2016 Task 2 5/27/2016 indenting-taskControl.java 6/1/2016 Banner 6/2/2016 StartProgramView 6/3/2016 MainMenuView

--- Hogwarts/src/hogwarts/Hogwarts.java
+++ Hogwarts/src/hogwarts/Hogwarts.java
@@ -14,89 +14,34 @@ import byui.cit260.Hogwarts.model.Character;
 import byui.cit260.Hogwarts.model.Item;
 import byui.cit260.Hogwarts.model.Map;
 import byui.cit260.Hogwarts.model.Game;
+import citbyui.cit260.Hogwarts.view.StartProgramView;
 
 public class Hogwarts {
-
+    
+    private static Game currentGame = null;
+    private static Player player = null;
+    
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Player playerOne = new Player();
-        
-        playerOne.setName("Fred Flintson");
-        
-        String playerInfo = playerOne.toString();
-        System.out.println(playerInfo);
-        
-        
-        House houseOne = new House();
-        
-        houseOne.setHouseName("Gryfindor");
-        
-        String houseInfo = houseOne.toString();
-        System.out.println(houseInfo);
-        
-        Location locationOne = new Location();
-        locationOne.setRow(1);
-        locationOne.setColumn(2);
-        locationOne.setVisited(true);
-        locationOne.setAmountRemaining(2);
-        String rowInfo = locationOne.toString();
-        System.out.println(rowInfo);
-        
-        Scene sceneOne = new Scene();
-        sceneOne.setDescription("Office");
-        sceneOne.setBlockedLocation(true);
-        sceneOne.setDisplaySymbol(true);
-        String sceneInfo = sceneOne.toString();
-        System.out.println(sceneInfo);
-        
-        TaskScene taskOne = new TaskScene();
-        taskOne.setAmountAvailable(23);
-        taskOne.setResourcesNeeded(15);
-        taskOne.setAttribute("Hickory");
-        String taskInfo = taskOne.toString();
-        System.out.println(taskInfo);
-        
-        
-
-        // character
-        Character characterOne = new Character();
-        
-        characterOne.setName("Harry Potter");
-        
-        String characterInfo = characterOne.toString();
-        System.out.println(characterInfo);
-       
-      
-        // item
-        Item itemOne = new Item();
-        
-        itemOne.setInventoryType("wands");
-        itemOne.setQuantityInStock(22);
-        
-        String inventoryInfo = itemOne.toString();
-        System.out.println(inventoryInfo);
-    
-        
-        // map
-        Map myMap = new Map();
-        
-        myMap.setRowCount(10);
-        myMap.setColumnCount(50);
-        
-        String mapInfo = myMap.toString();
-        System.out.println(mapInfo);
-        
-        
-        // game
-        Game myGame = new Game();
-        // need explanation of set start games type
-        myGame.setStartGame(20);
-        myGame.setEndGame(60);
-        
-        String gameInfo = myGame.toString();
-        System.out.println(gameInfo);
+        StartProgramView startProgramView = new StartProgramView();
+        startProgramView.displayStartProgramView();
+}
+    public static Player getPlayer() {
+        return player;
     }
     
+    public static void setPlayer(Player player) {
+        Hogwarts.player = player;
+    }
+
+    public static Game getCurrentGame() {
+        return currentGame;
+    }
+
+    public static void setCurrentGame(Game currentGame) {
+        Hogwarts.currentGame = currentGame;
+    }
+
 }
