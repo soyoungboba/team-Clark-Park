30f8f3389e28748f92f85049ed71de558613fd61
boba <boba@10.18.149.57>
2016-06-10 13:13:46 -0600

    6/10/2016 inventory control

---
+++ Hogwarts/src/byui/cit260/Hogwarts/control/InventoryControl.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.Hogwarts.control;
+import byui.cit260.Hogwarts.model.Player;
+import hogwarts.Hogwarts;
+/**
+ *
+ * @author boba
+ */
+public class InventoryControl {
+    
+}
