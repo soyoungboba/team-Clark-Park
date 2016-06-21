92d25b4aed705bbae97abf0efa6997d29128baa1
boba <boba@10.18.149.57>
2016-06-20 20:38:40 -0600

    6/20/2016 task 2

---
+++ Hogwarts/src/citbyui/cit260/Hogwarts/view/Task2View.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package citbyui.cit260.Hogwarts.view;
+import java.util.Random;
+import byui.cit260.Hogwarts.control.GameControl;
+import byui.cit260.Hogwarts.control.TaskControl;
+
+
+/**
+ *
+ * @author boba
+ */
+public class Task2View extends View {
+    
+    private double randOne;
+    private double randTwo;
+    private double randThree;
+    private double answer;
+    
+    public Task2View() {
+        super("\nPlease Enter Your Answer");
+        randOne = GameControl.randNum();
+        randTwo = GameControl.randNum();
+        randThree = GameControl.randNum();
+        System.out.println("\n Your challenge in this taks is to"
+                +"\n calculate the volume of rectangle"
+                +"\n that has a width of " + randOne + ", height"
+                +"\n of " + randTwo + ", and length of " 
+                +"\n randThree" + ".");
+    }
+}
