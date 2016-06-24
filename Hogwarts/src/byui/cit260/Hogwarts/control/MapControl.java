30afe8cdc2bd41aab4acb2429b23b98c99020539
boba <boba@10.18.149.57>
2016-06-24 13:48:18 -0600

    6/23/2016

--- Hogwarts/src/byui/cit260/Hogwarts/control/MapControl.java
+++ Hogwarts/src/byui/cit260/Hogwarts/control/MapControl.java
@@ -25,7 +25,7 @@ public class MapControl {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
-    private static class MapControlException extends Exception {
+    public static class MapControlException extends Exception {
 
         public MapControlException() {
         }
