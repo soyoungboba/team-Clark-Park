d0b82a573d152279c7ebfae7536a47f50bac6b39
boba <boba@10.18.149.57>
2016-06-24 12:44:45 -0600

    6/23/2016

--- Hogwarts/src/byui/cit260/Hogwarts/control/GameControl.java
+++ Hogwarts/src/byui/cit260/Hogwarts/control/GameControl.java
@@ -5,6 +5,9 @@
  */
 package byui.cit260.Hogwarts.control;
 
+import byui.cit260.Hogwarts.model.Game;
+import byui.cit260.Hogwarts.model.Item;
+import byui.cit260.Hogwarts.model.Map;
 import byui.cit260.Hogwarts.model.Player;
 import hogwarts.Hogwarts;
 import java.util.Random;
@@ -28,9 +31,28 @@ public class GameControl {
         return player;
     }
 
-    public static void createNewGame(Player player) {
-        System.out.println("\n*** createNewGame() function called ***"); 
+    public static void createNewGame(Player player) throws MapControl.MapControlException {
+        Game game = new Game(); //create new game
+        Hogwarts.setCurrentGame(game); //save in Hogwarts
+        
+        game.setPlayer(player); //save player in game
+        
+        //create the item and save in the game
+        Item[] itemList = GameControl.createItemList(); 
+        game.setItem(itemList);
+        
+        Map map = MapControl.createMap(); //create and initialize new map
+        game.setMap(map); //save map in game
+        
+        //move characters to starting position in the map
+        MapControl.moveCharactersToStartingLocation(map);
     }
+    
+    public static Item[] createItemList() {
+        System.out.println("\n*** called createItemList() in GameControl ***");
+        return null;
+    }
+    
     public static void saveHouse(String house) {
         System.out.println("\n*** saveHouse() function called ***");
     }
--- Hogwarts/src/byui/cit260/Hogwarts/control/MapControl.java
+++ Hogwarts/src/byui/cit260/Hogwarts/control/MapControl.java
@@ -4,6 +4,7 @@
  * and open the template in the editor.
  */
 package byui.cit260.Hogwarts.control;
+import byui.cit260.Hogwarts.model.Map;
 import byui.cit260.Hogwarts.model.Player;
 import hogwarts.Hogwarts;
 /**
@@ -11,5 +12,22 @@ import hogwarts.Hogwarts;
  * @author boba
  */
 public class MapControl {
-    
+    public static Map createMap() throws MapControlException {
+        //create the map
+        Map map = null;
+        
+        System.out.println("\n*** createMap() called ***");
+        
+        return map;
+    }
+
+    static void moveCharactersToStartingLocation(Map map) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    private static class MapControlException extends Exception {
+
+        public MapControlException() {
+        }
+    }
 }
