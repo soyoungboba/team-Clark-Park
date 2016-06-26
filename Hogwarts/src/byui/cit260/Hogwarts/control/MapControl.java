0985b11eb4cceb8beeb31cb004bed574defc4e00
boba <boba@10.18.149.57>
2016-06-25 21:25:00 -0600

    6/24/2016

--- Hogwarts/src/byui/cit260/Hogwarts/control/MapControl.java
+++ Hogwarts/src/byui/cit260/Hogwarts/control/MapControl.java
@@ -6,6 +6,8 @@
 package byui.cit260.Hogwarts.control;
 import byui.cit260.Hogwarts.model.Map;
 import byui.cit260.Hogwarts.model.Player;
+import byui.cit260.Hogwarts.model.Scene;
+import byui.cit260.Hogwarts.model.SceneType;
 import hogwarts.Hogwarts;
 /**
  *
@@ -14,15 +16,28 @@ import hogwarts.Hogwarts;
 public class MapControl {
     public static Map createMap() throws MapControlException {
         //create the map
-        Map map = null;
+        Map map = new Map(5,5);
         
-        System.out.println("\n*** createMap() called ***");
+        Scene[] scenes = createScenes();
+        
+        GameControl.assignScenesToLocations(map, scenes);
         
         return map;
     }
 
     static void moveCharactersToStartingLocation(Map map) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    private static Scene[] createScenes() {
+        Scene[] scenes = new Scene[SceneType.values().length];
+        
+        Scene startingScene = new Scene();
+        scenes[SceneType.start.ordinal()] = startingScene;
+        
+        Scene finish = new Scene();
+        scenes[SceneType.finish.ordinal()] = finish;
+        return scenes;
     }
 
     public static class MapControlException extends Exception {
