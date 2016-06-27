0c8c9835c80a898cdd96d3cb20f79988915e4440
boba <boba@10.18.149.57>
2016-06-27 13:18:16 -0600

    6/27/2016 added symbols

--- Hogwarts/src/byui/cit260/Hogwarts/control/MapControl.java
+++ Hogwarts/src/byui/cit260/Hogwarts/control/MapControl.java
@@ -36,7 +36,7 @@ public class MapControl {
         startingScene.setDescription(
                 "Professor McGonagall's office."
         );
-        startingScene.setMapSymbol("");
+        startingScene.setMapSymbol("MO");
         startingScene.setBlocked(false);
         scenes[SceneType.Office_of_Professor_McGonagall.ordinal()] = startingScene; //starting scene
         
@@ -44,7 +44,7 @@ public class MapControl {
         Gryffindor_Dormitory.setDescription(
                 "Gryffindor Dormitory."
         );
-        Gryffindor_Dormitory.setMapSymbol("");
+        Gryffindor_Dormitory.setMapSymbol("GD");
         Gryffindor_Dormitory.setBlocked(false);
         scenes[SceneType.Gryffindor_Dormitory.ordinal()] = startingScene; //scene 2
         
@@ -52,7 +52,7 @@ public class MapControl {
         Chamber_of_Secrets.setDescription(
                 "Chamber of Secrets."
         );
-        Chamber_of_Secrets.setMapSymbol("");
+        Chamber_of_Secrets.setMapSymbol("CS");
         Chamber_of_Secrets.setBlocked(false);
         scenes[SceneType.Chamber_of_Secrets.ordinal()] = startingScene; //scene 3
         
@@ -60,7 +60,7 @@ public class MapControl {
         The_Green_Houses.setDescription(
                 "The Green Houses."
         );
-        The_Green_Houses.setMapSymbol("");
+        The_Green_Houses.setMapSymbol("GH");
         The_Green_Houses.setBlocked(false);
         scenes[SceneType.The_Green_Houses.ordinal()] = startingScene; //scene 4
         
@@ -68,7 +68,7 @@ public class MapControl {
         Black_Lake.setDescription(
                 "Black Lake."
         );
-        Black_Lake.setMapSymbol("");
+        Black_Lake.setMapSymbol("BL");
         Black_Lake.setBlocked(false);
         scenes[SceneType.Black_Lake.ordinal()] = startingScene; //scene 5 
         
@@ -76,7 +76,7 @@ public class MapControl {
         Classroom.setDescription(
                 "Classroom."
         );
-        Classroom.setMapSymbol("");
+        Classroom.setMapSymbol("CR");
         Classroom.setBlocked(false);
         scenes[SceneType.Classroom.ordinal()] = startingScene; //scene 6 and 18
         
@@ -84,7 +84,7 @@ public class MapControl {
         Stairway.setDescription(
                 "Stairway."
         );
-        Stairway.setMapSymbol("");
+        Stairway.setMapSymbol("SW");
         Stairway.setBlocked(false);
         scenes[SceneType.Stairway.ordinal()] = startingScene; //scene 7
         
@@ -92,7 +92,7 @@ public class MapControl {
         Library.setDescription(
                 "Library."
         );
-        Library.setMapSymbol("");
+        Library.setMapSymbol("LB");
         Library.setBlocked(false);
         scenes[SceneType.Library.ordinal()] = startingScene; //scene 8
         
@@ -100,7 +100,7 @@ public class MapControl {
         Hut_of_Hagrid.setDescription(
                 "Hagrid's Hut."
         );
-        Hut_of_Hagrid.setMapSymbol("");
+        Hut_of_Hagrid.setMapSymbol("HH");
         Hut_of_Hagrid.setBlocked(false);
         scenes[SceneType.Hut_of_Hagrid.ordinal()] = startingScene; //scene 9
         
@@ -108,7 +108,7 @@ public class MapControl {
         Forbidden_Forest.setDescription(
                 "Forbidden Forest."
         );
-        Forbidden_Forest.setMapSymbol("");
+        Forbidden_Forest.setMapSymbol("FF");
         Forbidden_Forest.setBlocked(false);
         scenes[SceneType.Forbidden_Forest.ordinal()] = startingScene; //scene 10 and 15
         
@@ -116,7 +116,7 @@ public class MapControl {
         Office_of_Dumbledore.setDescription(
                 "Dumbledore's Office."
         );
-        Office_of_Dumbledore.setMapSymbol("");
+        Office_of_Dumbledore.setMapSymbol("DO");
         Office_of_Dumbledore.setBlocked(false);
         scenes[SceneType.Office_of_Dumbledore.ordinal()] = startingScene; //scene 11
         
@@ -124,7 +124,7 @@ public class MapControl {
         Hallway.setDescription(
                 "Hallway."
         );
-        Hallway.setMapSymbol("");
+        Hallway.setMapSymbol("HW");
         Hallway.setBlocked(false);
         scenes[SceneType.Hallway.ordinal()] = startingScene; //scene 12 and 19
         
@@ -132,7 +132,7 @@ public class MapControl {
         startingScene.setDescription(
                 "Great Hall."
         );
-        Great_Hall.setMapSymbol("");
+        Great_Hall.setMapSymbol("GH");
         Great_Hall.setBlocked(false);
         scenes[SceneType.Great_Hall.ordinal()] = startingScene; //scene 13
         
@@ -140,7 +140,7 @@ public class MapControl {
         Quidditch_Field.setDescription(
                 "Quidditch Field."
         );
-        Quidditch_Field.setMapSymbol("");
+        Quidditch_Field.setMapSymbol("QF");
         Quidditch_Field.setBlocked(false);
         scenes[SceneType.Quidditch_Field.ordinal()] = startingScene; //scene 14
         
@@ -156,7 +156,7 @@ public class MapControl {
         Lavatory_of_Girls.setDescription(
                 "Lavatory of girls."
         );
-        Lavatory_of_Girls.setMapSymbol("");
+        Lavatory_of_Girls.setMapSymbol("LG");
         Lavatory_of_Girls.setBlocked(false);
         scenes[SceneType.Lavatory_of_Girls.ordinal()] = startingScene; //scene 16
         
@@ -164,7 +164,7 @@ public class MapControl {
         Hospital_Wing.setDescription(
                 "Hospital Wing."
         );
-        Hospital_Wing.setMapSymbol("");
+        Hospital_Wing.setMapSymbol("HW");
         Hospital_Wing.setBlocked(false);
         scenes[SceneType.Hospital_Wing.ordinal()] = startingScene; //scene 17
         
@@ -188,7 +188,7 @@ public class MapControl {
         Front_Walk.setDescription(
                 "Front Walk."
         );
-        Front_Walk.setMapSymbol("");
+        Front_Walk.setMapSymbol("FW");
         Front_Walk.setBlocked(false);
         scenes[SceneType.Front_Walk.ordinal()] = startingScene; //scene 20
         
@@ -196,7 +196,7 @@ public class MapControl {
         Room_of_Requirement.setDescription(
                 "Room of Requiremente."
         );
-        Room_of_Requirement.setMapSymbol("");
+        Room_of_Requirement.setMapSymbol("RR");
         Room_of_Requirement.setBlocked(false);
         scenes[SceneType.Room_of_Requirement.ordinal()] = startingScene; //scene 21
         
@@ -204,7 +204,7 @@ public class MapControl {
         Office_of_Professor_Snape.setDescription(
                 "Professor Snape's office."
         );
-        Office_of_Professor_Snape.setMapSymbol("");
+        Office_of_Professor_Snape.setMapSymbol("SO");
         Office_of_Professor_Snape.setBlocked(false);
         scenes[SceneType.Office_of_Professor_Snape.ordinal()] = startingScene; //scene 22
         
@@ -212,7 +212,7 @@ public class MapControl {
         Slytherin_Dormitory.setDescription(
                 "Slytherin_Dormitory."
         );
-        Slytherin_Dormitory.setMapSymbol("");
+        Slytherin_Dormitory.setMapSymbol("SD");
         Slytherin_Dormitory.setBlocked(false);
         scenes[SceneType.Slytherin_Dormitory.ordinal()] = startingScene; // scene 23
         
@@ -220,7 +220,7 @@ public class MapControl {
         Dungeon.setDescription(
                 "Dungeon."
         );
-        Dungeon.setMapSymbol("");
+        Dungeon.setMapSymbol("DG");
         Dungeon.setBlocked(false);
         scenes[SceneType.Dungeon.ordinal()] = startingScene; // scene 24
         
@@ -228,7 +228,7 @@ public class MapControl {
         startingScene.setDescription(
                 "Congratulation! Well done."
         );
-        startingScene.setMapSymbol("");
+        startingScene.setMapSymbol("FN");
         startingScene.setBlocked(false);
         scenes[SceneType.finish.ordinal()] = finish; 
         return scenes;
