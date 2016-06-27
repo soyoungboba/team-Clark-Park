7ba8178b6ffd78c927991dfc4432c7d504c1324a
boba <boba@10.18.149.57>
2016-06-27 13:01:04 -0600

    6/27/2016 updated

--- Hogwarts/src/byui/cit260/Hogwarts/control/MapControl.java
+++ Hogwarts/src/byui/cit260/Hogwarts/control/MapControl.java
@@ -40,188 +40,188 @@ public class MapControl {
         startingScene.setBlocked(false);
         scenes[SceneType.Office_of_Professor_McGonagall.ordinal()] = startingScene; //starting scene
         
-        Scene startingScene = new Scene();
-        startingScene.setDescription(
+        Scene Gryffindor_Dormitory = new Scene();
+        Gryffindor_Dormitory.setDescription(
                 "Gryffindor Dormitory."
         );
-        startingScene.setMapSymbol("");
-        startingScene.setBlocked(false);
+        Gryffindor_Dormitory.setMapSymbol("");
+        Gryffindor_Dormitory.setBlocked(false);
         scenes[SceneType.Gryffindor_Dormitory.ordinal()] = startingScene; //scene 2
         
-        Scene startingScene = new Scene();
-        startingScene.setDescription(
+        Scene Chamber_of_Secrets = new Scene();
+        Chamber_of_Secrets.setDescription(
                 "Chamber of Secrets."
         );
-        startingScene.setMapSymbol("");
-        startingScene.setBlocked(false);
+        Chamber_of_Secrets.setMapSymbol("");
+        Chamber_of_Secrets.setBlocked(false);
         scenes[SceneType.Chamber_of_Secrets.ordinal()] = startingScene; //scene 3
         
-        Scene startingScene = new Scene();
-        startingScene.setDescription(
+        Scene The_Green_Houses = new Scene();
+        The_Green_Houses.setDescription(
                 "The Green Houses."
         );
-        startingScene.setMapSymbol("");
-        startingScene.setBlocked(false);
+        The_Green_Houses.setMapSymbol("");
+        The_Green_Houses.setBlocked(false);
         scenes[SceneType.The_Green_Houses.ordinal()] = startingScene; //scene 4
         
-        Scene startingScene = new Scene();
-        startingScene.setDescription(
+        Scene Black_Lake = new Scene();
+        Black_Lake.setDescription(
                 "Black Lake."
         );
-        startingScene.setMapSymbol("");
-        startingScene.setBlocked(false);
-        scenes[SceneType.Black_Lake.ordinal()] = startingScene; //scene 5
+        Black_Lake.setMapSymbol("");
+        Black_Lake.setBlocked(false);
+        scenes[SceneType.Black_Lake.ordinal()] = startingScene; //scene 5 
         
-        Scene startingScene = new Scene();
-        startingScene.setDescription(
+        Scene Classroom = new Scene();
+        Classroom.setDescription(
                 "Classroom."
         );
-        startingScene.setMapSymbol("");
-        startingScene.setBlocked(false);
-        scenes[SceneType.Classroom.ordinal()] = startingScene; //scene 6
+        Classroom.setMapSymbol("");
+        Classroom.setBlocked(false);
+        scenes[SceneType.Classroom.ordinal()] = startingScene; //scene 6 and 18
         
-        Scene startingScene = new Scene();
-        startingScene.setDescription(
+        Scene Stairway = new Scene();
+        Stairway.setDescription(
                 "Stairway."
         );
-        startingScene.setMapSymbol("");
-        startingScene.setBlocked(false);
+        Stairway.setMapSymbol("");
+        Stairway.setBlocked(false);
         scenes[SceneType.Stairway.ordinal()] = startingScene; //scene 7
         
-        Scene startingScene = new Scene();
-        startingScene.setDescription(
+        Scene Library = new Scene();
+        Library.setDescription(
                 "Library."
         );
-        startingScene.setMapSymbol("");
-        startingScene.setBlocked(false);
-//        scenes[SceneType.Library.ordinal()] = startingScene; //scene 8
+        Library.setMapSymbol("");
+        Library.setBlocked(false);
+        scenes[SceneType.Library.ordinal()] = startingScene; //scene 8
         
-        Scene startingScene = new Scene();
-        startingScene.setDescription(
+        Scene Hut_of_Hagrid = new Scene();
+        Hut_of_Hagrid.setDescription(
                 "Hagrid's Hut."
         );
-        startingScene.setMapSymbol("");
-        startingScene.setBlocked(false);
+        Hut_of_Hagrid.setMapSymbol("");
+        Hut_of_Hagrid.setBlocked(false);
         scenes[SceneType.Hut_of_Hagrid.ordinal()] = startingScene; //scene 9
         
-        Scene startingScene = new Scene();
-        startingScene.setDescription(
+        Scene Forbidden_Forest = new Scene();
+        Forbidden_Forest.setDescription(
                 "Forbidden Forest."
         );
-        startingScene.setMapSymbol("");
-        startingScene.setBlocked(false);
-        scenes[SceneType.Forbidden_Forest.ordinal()] = startingScene; //scene 10
+        Forbidden_Forest.setMapSymbol("");
+        Forbidden_Forest.setBlocked(false);
+        scenes[SceneType.Forbidden_Forest.ordinal()] = startingScene; //scene 10 and 15
         
-        Scene startingScene = new Scene();
-        startingScene.setDescription(
+        Scene Office_of_Dumbledore = new Scene();
+        Office_of_Dumbledore.setDescription(
                 "Dumbledore's Office."
         );
-        startingScene.setMapSymbol("");
-        startingScene.setBlocked(false);
+        Office_of_Dumbledore.setMapSymbol("");
+        Office_of_Dumbledore.setBlocked(false);
         scenes[SceneType.Office_of_Dumbledore.ordinal()] = startingScene; //scene 11
         
-        Scene startingScene = new Scene();
-        startingScene.setDescription(
+        Scene Hallway = new Scene();
+        Hallway.setDescription(
                 "Hallway."
         );
-        startingScene.setMapSymbol("");
-        startingScene.setBlocked(false);
-        scenes[SceneType.Hallway.ordinal()] = startingScene; //scene 12
+        Hallway.setMapSymbol("");
+        Hallway.setBlocked(false);
+        scenes[SceneType.Hallway.ordinal()] = startingScene; //scene 12 and 19
         
-        Scene startingScene = new Scene();
+        Scene Great_Hall = new Scene();
         startingScene.setDescription(
                 "Great Hall."
         );
-        startingScene.setMapSymbol("");
-        startingScene.setBlocked(false);
+        Great_Hall.setMapSymbol("");
+        Great_Hall.setBlocked(false);
         scenes[SceneType.Great_Hall.ordinal()] = startingScene; //scene 13
         
-        Scene startingScene = new Scene();
-        startingScene.setDescription(
+        Scene Quidditch_Field = new Scene();
+        Quidditch_Field.setDescription(
                 "Quidditch Field."
         );
-        startingScene.setMapSymbol("");
-        startingScene.setBlocked(false);
+        Quidditch_Field.setMapSymbol("");
+        Quidditch_Field.setBlocked(false);
         scenes[SceneType.Quidditch_Field.ordinal()] = startingScene; //scene 14
         
-        Scene startingScene = new Scene();
-        startingScene.setDescription(
+        /*Scene Forbidden_Forest = new Scene();
+        Forbidden_Forest.setDescription(
                 "Forbidden Forest."
         );
-        startingScene.setMapSymbol("");
-        startingScene.setBlocked(false);
-        scenes[SceneType.Forbidden_Forest.ordinal()] = startingScene; //scene 15
+        Forbidden_Forest.setMapSymbol("");
+        Forbidden_Forest.setBlocked(false);
+        scenes[SceneType.Forbidden_Forest.ordinal()] = startingScene; //scene 15*/
         
-        Scene startingScene = new Scene();
-        startingScene.setDescription(
+        Scene Lavatory_of_Girls = new Scene();
+        Lavatory_of_Girls.setDescription(
                 "Lavatory of girls."
         );
-        startingScene.setMapSymbol("");
-        startingScene.setBlocked(false);
+        Lavatory_of_Girls.setMapSymbol("");
+        Lavatory_of_Girls.setBlocked(false);
         scenes[SceneType.Lavatory_of_Girls.ordinal()] = startingScene; //scene 16
         
-        Scene startingScene = new Scene();
-        startingScene.setDescription(
+        Scene Hospital_Wing = new Scene();
+        Hospital_Wing.setDescription(
                 "Hospital Wing."
         );
-        startingScene.setMapSymbol("");
-        startingScene.setBlocked(false);
+        Hospital_Wing.setMapSymbol("");
+        Hospital_Wing.setBlocked(false);
         scenes[SceneType.Hospital_Wing.ordinal()] = startingScene; //scene 17
         
-        Scene startingScene = new Scene();
+        /*Scene Classroom = new Scene();
         startingScene.setDescription(
                 "Classroom."
         );
-        startingScene.setMapSymbol("");
-        startingScene.setBlocked(false);
+        Classroome.setMapSymbol("");
+        Classroom.setBlocked(false);
         scenes[SceneType.Classroom.ordinal()] = startingScene; // scene 18
         
-        Scene startingScene = new Scene();
-        startingScene.setDescription(
+        Scene Hallway = new Scene();
+        Hallway.setDescription(
                 "Hallway."
         );
-        startingScene.setMapSymbol("");
-        startingScene.setBlocked(false);
-        scenes[SceneType.Hallway.ordinal()] = startingScene; // scene 19
+        Hallway.setMapSymbol("");
+        Hallway.setBlocked(false);
+        scenes[SceneType.Hallway.ordinal()] = startingScene; // scene 19*/
         
-        Scene startingScene = new Scene();
-        startingScene.setDescription(
+        Scene Front_Walk = new Scene();
+        Front_Walk.setDescription(
                 "Front Walk."
         );
-        startingScene.setMapSymbol("");
-        startingScene.setBlocked(false);
+        Front_Walk.setMapSymbol("");
+        Front_Walk.setBlocked(false);
         scenes[SceneType.Front_Walk.ordinal()] = startingScene; //scene 20
         
-        Scene startingScene = new Scene();
-        startingScene.setDescription(
+        Scene Room_of_Requirement = new Scene();
+        Room_of_Requirement.setDescription(
                 "Room of Requiremente."
         );
-        startingScene.setMapSymbol("");
-        startingScene.setBlocked(false);
+        Room_of_Requirement.setMapSymbol("");
+        Room_of_Requirement.setBlocked(false);
         scenes[SceneType.Room_of_Requirement.ordinal()] = startingScene; //scene 21
         
-        Scene startingScene = new Scene();
-        startingScene.setDescription(
+        Scene Office_of_Professor_Snape = new Scene();
+        Office_of_Professor_Snape.setDescription(
                 "Professor Snape's office."
         );
-        startingScene.setMapSymbol("");
-        startingScene.setBlocked(false);
+        Office_of_Professor_Snape.setMapSymbol("");
+        Office_of_Professor_Snape.setBlocked(false);
         scenes[SceneType.Office_of_Professor_Snape.ordinal()] = startingScene; //scene 22
         
-        Scene startingScene = new Scene();
-        startingScene.setDescription(
+        Scene Slytherin_Dormitory = new Scene();
+        Slytherin_Dormitory.setDescription(
                 "Slytherin_Dormitory."
         );
-        startingScene.setMapSymbol("");
-        startingScene.setBlocked(false);
+        Slytherin_Dormitory.setMapSymbol("");
+        Slytherin_Dormitory.setBlocked(false);
         scenes[SceneType.Slytherin_Dormitory.ordinal()] = startingScene; // scene 23
         
-        Scene startingScene = new Scene();
-        startingScene.setDescription(
+        Scene Dungeon = new Scene();
+        Dungeon.setDescription(
                 "Dungeon."
         );
-        startingScene.setMapSymbol("");
-        startingScene.setBlocked(false);
+        Dungeon.setMapSymbol("");
+        Dungeon.setBlocked(false);
         scenes[SceneType.Dungeon.ordinal()] = startingScene; // scene 24
         
         Scene finish = new Scene();
