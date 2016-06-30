7f832197d6a59775360a8a43e19792b24cbc3f31
boba <boba@10.18.149.57>
2016-06-30 12:06:55 -0600

    6/30/2016 Map control exception

--- Hogwarts/src/citbyui/cit260/Hogwarts/exceptions/MapControlException.java
+++ Hogwarts/src/citbyui/cit260/Hogwarts/exceptions/MapControlException.java
@@ -9,6 +9,25 @@ package citbyui.cit260.Hogwarts.exceptions;
  *
  * @author boba
  */
-public class MapControlException {
+public class MapControlException extends Exception {
+
+    public MapControlException() {
+    }
+
+    public MapControlException(String message) {
+        super(message);
+    }
+
+    public MapControlException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public MapControlException(Throwable cause) {
+        super(cause);
+    }
+
+    public MapControlException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
     
 }
