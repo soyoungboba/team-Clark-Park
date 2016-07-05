516fe9a597e5badfd5300854b3db97286f4b0709
boba <boba@10.18.149.57>
2016-07-05 17:35:34 -0600

    7/5/2016 lesson 11 team assignment

---
+++ Hogwarts/src/citbyui/cit260/Hogwarts/view/ErrorView.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package citbyui.cit260.Hogwarts.view;
+
+import hogwarts.Hogwarts;
+import java.io.PrintWriter;
+
+/**
+ *
+ * @author boba
+ */
+public class ErrorView {
+    
+    private static final PrintWriter errorFile = Hogwarts.getoutFile();
+    
+    public static void display(String className, String errorMessage) {
+        errorFile.println(
+                    "----------------------------------------------------"
+                   +"\n- ERROR - " + errorMessage
+                   +"\n--------------------------------------------------");
+    }
+}
