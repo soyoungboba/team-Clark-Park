b606328ef84fad9137fc83c62ade71bb089eba05
boba <boba@10.18.149.57>
2016-07-05 17:05:00 -0600

    7/5/2016 lesson 11 team assignment

--- Hogwarts/src/hogwarts/Hogwarts.java
+++ Hogwarts/src/hogwarts/Hogwarts.java
@@ -17,26 +17,63 @@ import byui.cit260.Hogwarts.model.Game;
 import citbyui.cit260.Hogwarts.view.StartProgramView;
 import citbyui.cit260.Hogwarts.view.HelpMenuView;
 import citbyui.cit260.Hogwarts.view.MainMenuView;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 
 public class Hogwarts {
     
     private static Game currentGame = null;
     private static Player player = null;
     private static House houseName = null;
+    
+    private static PrintWriter outFile = null;
+
+    public static PrintWriter getOutFile() {
+        return outFile;
+    }
+
+    public static void setOutFile(PrintWriter outFile) {
+        Hogwarts.outFile = outFile;
+    }
+
+    public static BufferedReader getInFile() {
+        return inFile;
+    }
+
+    public static void setInFile(BufferedReader inFile) {
+        Hogwarts.inFile = inFile;
+    }
+    private static BufferedReader inFile = null;
+    
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        
+        try {   
+        
+        // open character stream files for end user input and output
+        Hogwarts.inFile = 
+                new BufferedReader(new InputStreamReader(System.in));
+        
+        Hogwarts.outFile = new PrintWriter(System.out, true);
+        
+        // create StartProgramView and start the program
         StartProgramView startProgramView = new StartProgramView();
-        try{
-        startProgramView.displayStartProgramView();
+        startProgramView.display();
+        return;
 
-        }catch (Throwable te) {
-            System.out.println(te.getMessage());
+        } catch (Throwable te) {
+            
+            System.out.println("Exception: " + te.toString() +
+                               "\nCause: " + te.getCause() +
+                               "\nMessage: " + te.getMessage());
+           
             te.printStackTrace();
-            startProgramView.displayStartProgramView();
         }  
-}
+    }
+    
     public static Player getPlayer() {
         return player;
     }
